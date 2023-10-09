79639cd977a57620a2a3b576915c8c7f1ae7d48f
aeronka <Natalja-055@yandex.ru>
2023-10-09 11:43:56 +0300

    Пакеты, цены и предзапись

---
+++ animation.js
@@ -0,0 +1,21 @@
+function ready() {
+    var animateButton = function(e) {
+
+      e.preventDefault;
+      //reset animation
+      e.target.classList.remove('animate');
+      
+      e.target.classList.add('animate');
+      setTimeout(function(){
+        e.target.classList.remove('animate');
+      },700);
+    };
+
+    var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+    for (var i = 0; i < bubblyButtons.length; i++) {
+      bubblyButtons[i].addEventListener('click', animateButton);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ready);
